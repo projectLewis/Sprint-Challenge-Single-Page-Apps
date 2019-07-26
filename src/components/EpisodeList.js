5703432d889f4c4e5bef029432bf7db27297ca6f
Seth Lewis <lewiscatenary@gmail.com>
2019-07-26 11:57:47 -0500

    styling for episode list complete

--- src/components/EpisodeList.js
+++ src/components/EpisodeList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import EpisodeCard from "./EpisodeCard";
 
 const EpisodeList = () => {
   // TODO: Add useState to track data from useEffect
@@ -22,13 +23,12 @@ const EpisodeList = () => {
       {episodes ? (
         episodes.map(episode => {
           return (
-            <div style={{ border: "1px solid blue" }} key={episode.id}>
-              Episode: {episode.name}
-              <br />
-              Aired: {episode.air_date}
-              <br />
-              Episode Number: {episode.episode}
-            </div>
+            <EpisodeCard
+              key={episode.id}
+              name={episode.name}
+              airdate={episode.air_date}
+              episodeNumber={episode.episode}
+            />
           );
         })
       ) : (
